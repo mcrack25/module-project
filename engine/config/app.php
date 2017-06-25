def76e5d9efe04c4d76de4a46aae46fdd496cf7a
mcrack <mcrack@yandex.ru>
2017-06-25 09:02:59 +0300

    Немного отформатировал app.php

--- engine/config/app.php
+++ engine/config/app.php
@@ -1,35 +1,35 @@
 <?php
 
-function request_url(){
-    $result = ''; // Пока результат пуст
-    $default_port = 80; // Порт по-умолчанию
+    function request_url(){
+        $result = ''; // Пока результат пуст
+        $default_port = 80; // Порт по-умолчанию
 
-    // А не в защищенном-ли мы соединении?
-    if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS']=='on')) {
-        // В защищенном! Добавим протокол...
-        $result .= 'https://';
-        // ...и переназначим значение порта по-умолчанию
-        $default_port = 443;
-    } else {
-        // Обычное соединение, обычный протокол
-        $result .= 'http://';
+        // А не в защищенном-ли мы соединении?
+        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS']=='on')) {
+            // В защищенном! Добавим протокол...
+            $result .= 'https://';
+            // ...и переназначим значение порта по-умолчанию
+            $default_port = 443;
+        } else {
+            // Обычное соединение, обычный протокол
+            $result .= 'http://';
+        }
+        // Имя сервера, напр. site.com или www.site.com
+        $result .= $_SERVER['SERVER_NAME'];
+
+        // А порт у нас по-умолчанию?
+        if ($_SERVER['SERVER_PORT'] != $default_port) {
+            // Если нет, то добавим порт в URL
+            $result .= ':'.$_SERVER['SERVER_PORT'];
+        }
+
+        $result .= str_replace("/index.php", "", $_SERVER['PHP_SELF']);
+
+        // Последняя часть запроса (путь и GET-параметры).
+        //$result .= $_SERVER['REQUEST_URI'];
+        // Уфф, вроде получилось!
+        return $result;
     }
-    // Имя сервера, напр. site.com или www.site.com
-    $result .= $_SERVER['SERVER_NAME'];
-
-    // А порт у нас по-умолчанию?
-    if ($_SERVER['SERVER_PORT'] != $default_port) {
-        // Если нет, то добавим порт в URL
-        $result .= ':'.$_SERVER['SERVER_PORT'];
-    }
-
-    $result .= str_replace("/index.php", "", $_SERVER['PHP_SELF']);
-
-    // Последняя часть запроса (путь и GET-параметры).
-    //$result .= $_SERVER['REQUEST_URI'];
-    // Уфф, вроде получилось!
-    return $result;
-}
 
 return [
 
